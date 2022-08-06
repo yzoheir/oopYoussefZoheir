888f409fd66ca425bf380d3daf63859e63cf85f0
yzoheir <110730449+yzoheir@users.noreply.github.com>
2022-08-06 23:43:58 +0930

    Add main-1-2.cpp

---
+++ main-1-2.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include <iostream>
+extern int is_identity(int array[10][10]);
+int main() {
+  int array[10][10] = {
+      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+  };
+
+  int answer = is_identity(array);
+  if (answer == 1)
+
+  {
+    std::cout << "This is an Identity Matrix" << std::endl;
+
+  }
+
+  else
+
+  {
+    std::cout << "This is not an Identity Matrix" << std::endl;
+  }
+}
