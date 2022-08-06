8ba8b568c7c3cbe3043ba37991ea95624dd81676
yzoheir <110730449+yzoheir@users.noreply.github.com>
2022-08-06 23:44:25 +0930

    Add main-1-4.cpp

---
+++ main-1-4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+extern void print_scaled(int [3][3],int scale);
+int main()
+{
+    int scale=3;
+    int threebythree[3][3]={{0,1,2},{3,4,5},{6,7,8}};
+  print_scaled(threebythree,scale); 
+
+    return 0;
+}
